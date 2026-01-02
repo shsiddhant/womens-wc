4e0c7a050d052b982a8b4fee2f9fe55cacf9c1a1
Siddhant Sharma <sharmasiddhant@protonmail.com>
2026-01-02 14:59:45 +0530

    perf: replace vectorized get_match_data function with list comprehension.

--- src/womenswc/build_base_dataset.py
+++ src/womenswc/build_base_dataset.py
@@ -50,7 +50,7 @@ def matchdict(
 
 
 # Read match data from JSON
-def get_match_data_(
+def get_match_data(
     match_json: Path, city_to_country: dict
 ) -> dict[str, int | str | None]:
     match_id = match_json.name.removesuffix(".json")
@@ -60,23 +60,27 @@ def get_match_data_(
 
 # Build Dataset
 def build_db(
-    get_match_data_vec: Callable,
+    get_match_data: Callable,
     city_to_country,
-    historial_datadir: Path = HISTORICAL_DATA,
+    historical_datadir: Path = HISTORICAL_DATA,
 ):
-    json_files = list(historial_datadir.glob("*.json"))
+    json_files = list(historical_datadir.glob("*.json"))
     base_df = pd.DataFrame.from_records(
-        get_match_data_vec(json_files, city_to_country)
+        [get_match_data(match_json, city_to_country) for match_json in json_files]
     ).dropna()
     base_df["start_date"] = pd.to_datetime(base_df.start_date)
     base_df = base_df.sort_values(by=["start_date"])
     base_df = base_df[base_df.start_date.dt.year >= 2022]
-    base_df = base_df.reset_index()
-    base_df = base_df.drop(columns=["index"])
+    base_df = base_df.reset_index(drop=True)
     return base_df
 
-# Randomly Swap Labels
 def swap_labels(base_df: pd.DataFrame, seed: int = 1712):
+    """
+    Randomly swap labels to avoid bias in modeling, which
+    is expected since Australia are always first alphabetically,
+    and are the strongest team amongst women's cricket teams
+    by a fair margin.
+    """
     df = base_df.copy()
     rng = np.random.default_rng(seed=seed)
     mask = pd.Series(rng.choice([True, False], size=len(df)), index=df.index)
@@ -92,9 +96,8 @@ def swap_labels(base_df: pd.DataFrame, seed: int = 1712):
 def main():
     with open(DATA_DIRECTORY / "city-to-country.json", "r") as fp:
         city_to_country = json.load(fp)
-    get_match_data_vec = np.vectorize(get_match_data_, otypes=[dict])
-    base_df = build_db(get_match_data_vec, city_to_country)
-    base_df = swap_labels(base_df, 17120512)
+    base_df = build_db(get_match_data, city_to_country)
+    base_df = swap_labels(base_df, 171205121729)
     base_df.to_parquet(DATA_DIRECTORY / "processed" / "base_dataset.parquet")
     print(
         "Base dataset created and saved to "
