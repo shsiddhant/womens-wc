2e5565eb613352141a8f4802b353d81be8442df4
Siddhant Sharma <sharmasiddhant@protonmail.com>
2026-03-10 00:39:40 +0530

    notebook: first draft for introduction and baseline model - logistic regression

---
+++ src/womenswc/model_utils.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+import numpy as np
+from sklearn.metrics import confusion_matrix
+from sklearn.linear_model import LogisticRegression
+
+if TYPE_CHECKING:
+    from typing import Literal
+    import pandas as pd
+
+
+def train_validate_test(
+    features: pd.DataFrame,
+    target: pd.DataFrame,
+    cutoffs: pd.DatetimeIndex,
+    drop_cols: list[str]
+    ):
+    Index_train = features.start_date < cutoffs[0]
+    Index_test = (
+        (features.start_date >= cutoffs[0]) & (features.start_date <= cutoffs[1])
+    )
+    return (
+        features[Index_train].drop(columns=drop_cols),
+        target[Index_train].result,
+        features[Index_test].drop(columns=drop_cols),
+        target[Index_test].result
+    )
+
+def log_loss(
+    log_prob_matrix: np.ndarray[tuple[int, Literal[2]], np.dtype[np.float64]],
+    y_test: np.ndarray[tuple[int]] | pd.Series
+    ):
+    """
+    Calculate Log Loss from Log Probability Matrix for binary classification problems.
+    """
+    loss = - np.mean(
+        y_test * log_prob_matrix[:, 1]
+        + (1 - y_test) * log_prob_matrix[:, 0]
+        )
+    return loss
+
+def cross_validation_logisreg(
+    X_train: np.ndarray[tuple[int, int]],
+    Y_train: pd.Series,
+    n_parts: int = 9,
+    C: int = 1
+    ):
+    if not isinstance(n_parts, int) or n_parts <= 0:
+        raise ValueError("n_parts must a positive integer.")
+    size = len(X_train) // n_parts
+    indices = [size * k for k in range(1, n_parts, 1)]
+    prob_matrices = []
+    conf_matrices = []
+    log_loss_values = []
+    y_pred_values = []
+    for i in indices:
+        model = LogisticRegression(solver="lbfgs", max_iter=1000, C=C)
+        x_train, y_train = X_train[:i], Y_train.iloc[:i]
+        x_test, y_test = X_train[i:i + size], Y_train.iloc[i:i+size]
+
+        model.fit(x_train, y_train)
+
+        y_pred = model.predict(x_test)
+        y_pred_values.append(y_pred)
+        prob_matrix = model.predict_proba(x_test)
+        prob_matrices.append(prob_matrix)
+        log_loss_values.append(log_loss(np.log(prob_matrix), y_test))
+        conf_matrices.append(confusion_matrix(y_test, y_pred))
+    return conf_matrices, prob_matrices, log_loss_values, y_pred_values
